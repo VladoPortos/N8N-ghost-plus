4b4b52b9edc1382773deac304f19901a54bf9955
Vladimir Strycek <vladoportos@gmail.com>
2025-02-24 16:38:25 +0100

    fix: update credentials paths

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
         require('./dist/nodes/GhostV2/Ghostv2.node')
     ],
     credentials: [
-        require('./dist/credentials/GhostV2Api.credentials')
+        require('./dist/credentials/GhostV2AdminApi.credentials'),
+        require('./dist/credentials/GhostV2ContentApi.credentials')
     ]
 };
